9e695eb7295fd10098411fc089685bcf726687ec
Adrian Imioło <105633316+adrian-imiolo@users.noreply.github.com>
2023-05-23 22:02:09 +0200

    Comment to next task

--- src/QuizPage.jsx
+++ src/QuizPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function QuizPage(props) {
   const quizElements = props.quizArray.map((item) => {
-    return;
+    // do something to render quiz with 5 questions  on quizPage
     <h1>{item}</h1>;
   });
   return <div>{quizElements}</div>;
