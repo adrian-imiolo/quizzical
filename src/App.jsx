68cdafb8cb12c27dc90df3b27f057276bd676e25
Adrian Imioło <105633316+adrian-imiolo@users.noreply.github.com>
2023-05-29 12:42:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from "react";
 import StartingPage from "../src/StartingPage";
 import QuizPage from "../src/QuizPage";
+import { v4 as uuidv4 } from "uuid";
 import { decode } from "html-entities";
 
 function App() {
-  const [quiz, setQuiz] = useState([]);
+  const [quizData, setQuizData] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [isQuizStarted, setIsQuizStarted] = useState(false);
   const [isPlayedAgain, setIsPlayAgain] = useState(false);
 
-  useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5")
-      .then((res) => res.json())
-      .then((data) => setQuiz(data.results));
-  }, [isQuizStarted, isPlayedAgain]);
+  // useEffect(() => {
+  //   fetch("https://opentdb.com/api.php?amount=5")
+  //     .then((res) => res.json())
+  //     .then((data) => (data.results));
+  // }, []);
 
   useEffect(() => {
-    const extractedQuestions = quiz.map((item) => item.question);
-    setQuestions(extractedQuestions);
-  }, [quiz]);
+    async function fetchData() {
+      const questions = await getQuestions();
+      setQuestions(questions);
+    }
+    fetchData();
+  }, []);
+
+  async function fetchTriviaDB() {
+    const response = await fetch("https://opentdb.com/api.php?amount=5");
+    const data = await response.json();
+    return data.results;
+  }
+  // useEffect(() => {
+  //   const extractedQuestions = quiz.map((item) => item.question);
+  //   setQuestions(extractedQuestions);
+  // }, [quiz]);
 
   function startQuiz() {
     if (isPlayedAgain === false) {
@@ -39,16 +53,37 @@ function App() {
     }
     return array;
   }
+
+  async function getQuestions() {
+    const data = await fetchTriviaDB();
+
+    const questions = data.map((question) => {
+      const decodedQuestion = decode(question.question);
+      const decodedCorrectAnswer = decode(question.correct_answer);
+      const decodedIncorrectAnswers = question.incorrect_answers.map(decode);
+      const answers = shuffleArray([
+        ...decodedIncorrectAnswers,
+        decodedCorrectAnswer,
+      ]);
+      return {
+        id: uuidv4(),
+        question: decodedQuestion,
+        answers: answers,
+        correct_answer: decodedCorrectAnswer,
+      };
+    });
+    return questions;
+  }
   // ask chatGPT this question and you will receive good answer: ok, i'm doing quiz app, i'm fetching data from TRIVIA DB, i want to render 5 questions which i get from API, all questions have multiple answers i want to add below the question as separate buttons. i already know how to get data form API but i wonder what's the best way to make some function that creates some array of objects tha contains id, question and array of answers. then i will map through this array of objects to display data. do u think it's good idea? can u show me how to do it or do it better?
 
   // maybe transform this function to make whole object(or array of objects because u need to get 5 elements from API) which contains: array of answers, question, and id
 
-  function createAnswersArray() {
-    const answersArray = quiz.map((item) =>
-      shuffleArray([...item.incorrect_answers, item.correct_answer])
-    );
-    return answersArray;
-  }
+  // function createAnswersArray() {
+  //   const answersArray = quiz.map((item) =>
+  //     shuffleArray([...item.incorrect_answers, item.correct_answer])
+  //   );
+  //   return answersArray;
+  // }
 
   // make a function that gets data from quiz
   // make a function that adds correct answers to incorrect answers array and then shuffles this array
@@ -62,13 +97,27 @@ function App() {
       {isQuizStarted === false ? (
         <StartingPage startQuiz={startQuiz} />
       ) : (
-        <QuizPage quiz={quiz} answers={createAnswersArray()} />
+        <div>
+          {questions.map((question) => (
+            <div key={question.id}>
+              <h2>{question.question}</h2>
+              {question.answers.map((answer, index) => (
+                <button
+                  key={index}
+                  onClick={() =>
+                    handleAnswerClick(answer, question.correct_answer)
+                  }
+                >
+                  {answer}
+                </button>
+              ))}
+              <hr />
+            </div>
+          ))}
+        </div>
       )}
-      {/* {getQuestions()} */}
       {console.log(questions)}
-      {console.log(quiz)}
-      {console.log(createAnswersArray())}
-      {/* this fetchData() is only for now to make QuizPage render */}
+      {console.log(quizData)}
     </div>
   );
 }
