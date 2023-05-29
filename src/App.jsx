9e2f5f518026c896eeb93c99ce7f22dec6061902
Adrian Imioło <105633316+adrian-imiolo@users.noreply.github.com>
2023-05-29 13:07:27 +0200

    radio buttons
    made radio buttons, now it's time for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import StartingPage from "../src/StartingPage";
 import QuizPage from "../src/QuizPage";
+import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 import { decode } from "html-entities";
 
 function App() {
-  const [quizData, setQuizData] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [isQuizStarted, setIsQuizStarted] = useState(false);
-  const [isPlayedAgain, setIsPlayAgain] = useState(false);
+  const [isPlayedAgain, setIsPlayedAgain] = useState(false);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
 
   // useEffect(() => {
   //   fetch("https://opentdb.com/api.php?amount=5")
@@ -42,7 +43,7 @@ function App() {
 
   function playAgain() {
     if (isQuizStarted) {
-      setIsPlayAgain((prevIsPlayedAgain) => !prevIsPlayedAgain);
+      setIsPlayedAgain((prevIsPlayedAgain) => !prevIsPlayedAgain);
     }
   }
 
@@ -74,23 +75,13 @@ function App() {
     });
     return questions;
   }
-  // ask chatGPT this question and you will receive good answer: ok, i'm doing quiz app, i'm fetching data from TRIVIA DB, i want to render 5 questions which i get from API, all questions have multiple answers i want to add below the question as separate buttons. i already know how to get data form API but i wonder what's the best way to make some function that creates some array of objects tha contains id, question and array of answers. then i will map through this array of objects to display data. do u think it's good idea? can u show me how to do it or do it better?
 
-  // maybe transform this function to make whole object(or array of objects because u need to get 5 elements from API) which contains: array of answers, question, and id
-
-  // function createAnswersArray() {
-  //   const answersArray = quiz.map((item) =>
-  //     shuffleArray([...item.incorrect_answers, item.correct_answer])
-  //   );
-  //   return answersArray;
-  // }
-
-  // make a function that gets data from quiz
-  // make a function that adds correct answers to incorrect answers array and then shuffles this array
-  // display those answers on the page
-  // this answers should display in radios(u can only select 1 item at a time)
-
-  //  function that gets the quiz questions from the API
+  function handleAnswerChange(questionId, selectedAnswer) {
+    setSelectedAnswers((prevSelectedAnswers) => ({
+      ...prevSelectedAnswers,
+      [questionId]: selectedAnswer,
+    }));
+  }
 
   return (
     <div>
@@ -102,14 +93,16 @@ function App() {
             <div key={question.id}>
               <h2>{question.question}</h2>
               {question.answers.map((answer, index) => (
-                <button
-                  key={index}
-                  onClick={() =>
-                    handleAnswerClick(answer, question.correct_answer)
-                  }
-                >
+                <label key={index}>
+                  <input
+                    type="radio"
+                    name={question.id}
+                    value={answer}
+                    checked={selectedAnswers[question.id] === answer}
+                    onChange={() => handleAnswerChange(question.id, answer)}
+                  />
                   {answer}
-                </button>
+                </label>
               ))}
               <hr />
             </div>
@@ -117,7 +110,6 @@ function App() {
         </div>
       )}
       {console.log(questions)}
-      {console.log(quizData)}
     </div>
   );
 }
